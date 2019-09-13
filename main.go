9a17ad1a875e20cc359a3491f8af72901999aa90
benibana2001 <benibana2001@gmail.com>
2019-09-14 07:14:18 +0900

    walk dir

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rwcarlsen/goexif/mknote"
 	"github.com/rwcarlsen/goexif/tiff"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -21,7 +22,7 @@ type Data struct {
 }
 
 // 単一の画像ファイルを読み込み その情報を返す
-func readImg(path string) *Info{
+func readImg(path string) *Info {
 	// img情報を保持する構造体を作成
 	info := Info{
 		Path: path,
@@ -58,8 +59,8 @@ func readImg(path string) *Info{
 	info.Data.CamModel = m
 	info.Data.DateTime = tm
 
-	fmt.Printf("camModel: %v\n", info.Data.CamModel)
-	fmt.Printf("Taken: %v\n", info.Data.DateTime)
+	//fmt.Printf("camModel: %v\n", info.Data.CamModel)
+	//fmt.Printf("Taken: %v\n", info.Data.DateTime)
 
 	// 正規表現を使用してマッチを確認
 	/*
@@ -80,9 +81,52 @@ func readImg(path string) *Info{
 }
 
 func main() {
-	i := readImg("testdata/img01.jpg")
-	fmt.Printf("img: %v\n", i)
+	paths := getPath("testdata/")
+
+	for _, path := range paths {
+		img := readImg(path)
+		//fmt.Println(img)
+		//fmt.Printf("img: %v\n", img)
+
+		// ファイル名を変更する
+		// 新ファイル名 "日時" + "旧ファイル名"
+		var fNames = map[string]string{
+			"DateTime": img.Data.DateTime.String()[:10] + "-",
+			"Model": (func() string {
+				m := img.Data.CamModel
+				// Model値がnilでない場合は文字列化して返す
+				if m != nil {
+					//r := regexp.MustCompile()
+					return m.String() + "-"
+				} else {
+					return ""
+				}
+			})(),
+		}
+		fName := fNames["DateTime"] + fNames["Model"] + filepath.Base(path)
+		fmt.Println(fName)
+	}
+
+	//i := readImg("testdata/img01.jpg")
+	//fmt.Printf("img: %v\n", i)
+}
+
+// ディレクトリにある全ての.jpgのファイルパスを取得する
+func getPath(dirname string) []string {
+	var s []string
 
 	// ディレクトリを探索
+	err := filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".jpg" {
+			s = append(s, path)
+		}
+		return nil
+	})
 
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return s
 }
+
