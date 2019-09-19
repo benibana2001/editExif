b5e0e08098c93c8a0580389dfa2980f7bcd7c4fd
benibana2001 <benibana2001@gmail.com>
2019-09-19 12:50:54 +0900

    new file decoder

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/rwcarlsen/goexif/exif"
-	"github.com/rwcarlsen/goexif/mknote"
-	"github.com/rwcarlsen/goexif/tiff"
 	"os"
 	"path/filepath"
 	"regexp"
-	"time"
 )
 
 // ファイルを編集する構造体
@@ -30,18 +25,6 @@ type Options struct {
 	delNum   int
 	filter   string
 	dirBreak bool
-}
-
-// img情報を保持する構造体
-type Info struct {
-	path string
-	Data
-}
-
-// img情報を保持する構造体
-type Data struct {
-	camModel *tiff.Tag
-	dateTime time.Time
 }
 
 func main() {
@@ -156,96 +139,4 @@ func (e *Editor) newPath(name string, path string) string {
 		newDir := filepath.Dir(path) + "/"
 		return filepath.Join(newDir + name)
 	}
-}
-
-// 単一の画像ファイルを読み込み その情報を返す
-func readImg(path string) (*Info, error) {
-	// img情報を保持する構造体を作成
-	info := Info{
-		path: path,
-	}
-
-	// imgファイルを開く
-	f, err := os.Open(info.path)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer f.Close()
-
-	// おまじない...
-	exif.RegisterParsers(mknote.All...)
-
-	// xは *exif.Exif 型
-	x, errDecode := exif.Decode(f)
-	if errDecode != nil {
-		// xがnilの時 exifが存在しないケース
-		if x == nil {
-			// エラーを返して離脱
-			return nil, errors.New("画像情報が存在しません")
-		} else {
-			fmt.Println(err)
-		}
-	}
-
-	// Camera Model 取得
-	m, errModel := x.Get(exif.Model)
-	if errModel != nil {
-		fmt.Printf("Failed to read Camera Model: %v\n", errModel)
-	}
-
-	// dateTime 取得
-	tm, errDateTime := x.DateTime()
-
-	if errDateTime != nil {
-		fmt.Printf("Failed to read dateTime: %v\n", errDateTime)
-	}
-
-	// 構造体にセット
-	info.camModel = m
-	info.dateTime = tm
-	return &info, nil
-}
-
-// 対象のディレクトリ内の全ての.jpgに対して関数を実行する
-func iterateFunc(dir string, filter string, f func(string)) {
-	paths := getPath(dir, filter)
-
-	for _, path := range paths {
-		f(path)
-	}
-}
-
-// ディレクトリにある全ての.jpgのファイルパスを取得する
-func getPath(dirname string, filter string) []string {
-	var s []string
-
-	// ディレクトリを探索
-	err := filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
-		r := regexp.MustCompile(filter)
-
-		threshold := []string{".jpg", ".JPG"}
-		//if filepath.Ext(path) == ".jpg" && r.MatchString(filepath.Base(path)){
-		if isMatch(filepath.Ext(path), threshold) && r.MatchString(filepath.Base(path)) {
-			s = append(s, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return s
-}
-
-// 文字列のマッチングの合否を判定
-func isMatch(needle string, threshold []string) bool {
-	for _, val := range threshold {
-		if needle == val {
-			return true
-		}
-	}
-
-	return false
 }
